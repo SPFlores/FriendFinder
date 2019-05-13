40997bdf28fca744e65d005d2506ab3d571ef1db
SPFlores <sflores92886@gmail.com>
2019-05-12 19:10:04 -0700

    linking, questions boilerplate

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,7 @@
 // GET /api/friends to display JSON of all possible friends
 // POST /api/friends to handle incoming survey results, also compatibility logic
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+})
