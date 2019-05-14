7eaf43d990dcdc413ebf9d063e24fb2dacad9497
SPFlores <sflores92886@gmail.com>
2019-05-13 20:04:03 -0700

    pulling value from questions

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,14 @@
 // GET /api/friends to display JSON of all possible friends
 // POST /api/friends to handle incoming survey results, also compatibility logic
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems, options);
+document.addEventListener('click', e => {
+  e.preventDefault()
+  if (e.target.id === 'submitBtn') {
+    if ()
+    let answerArr = []
+    for (let i = 0; i < 10; i++) {
+      answerArr[i] = document.getElementById(`q${i + 1}`).value
+    }
+    console.log(answerArr)
+  }
 })
