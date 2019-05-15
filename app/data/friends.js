c0c162f1644b56d14aa8304e0ff182e997e120f1
SPFlores <sflores92886@gmail.com>
2019-05-14 17:07:51 -0700

    importing users, working on routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,36 @@
-// save all app data of new people in this file
+// save all app data of new people in this file as an array of object
 
-// {
-//   "name": "Ahmed",
-//     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//       "scores": [
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-// }
+exports.users = [
+  {
+    name: 'Ahmed',
+    photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    name: 'Bast',
+    photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,28 +1,49 @@
-module.exports = app => {
-  // GET /api/friends to display JSON of all possible friends
-  app.get('/api/friends', (req, res) => {
+// const { fetch, alert } = window
+// require users from the friends.js file
+const users = require('../data/friends.js')
 
+console.log(users)
+
+module.exports = app => {
+  app.get('/api/friends', (req, res) => {
+    // display JSON of all possible friends
   })
 
-  const compare = _ => {
+  const compare = user1 => {
+    // pull in users
+    // loop over it
+    // find totalDifference
+    // find which is the smallest difference
 
+    // users.forEach(user2 => {
+    //   let differenceArr = []
+    //   for (let i = 0; i < user2.length; i++) {
+    //     differenceArr.push(Math.abs(user2[i] - user1[i]))
+    //   }
+    //   let totalDifference = differenceArr.reduce()
+    // })
   }
 
+  app.post('/api/friends', (req, res) => {
+
+  })
   // POST /api/friends to handle incoming survey results, also compatibility logic
   document.querySelector('#submitBtn').addEventListener('click', e => {
     e.preventDefault()
-    const user = {}
     if (e.target.id === 'submitBtn') {
       if (!document.querySelector('#name').value || !document.querySelector('#photo').value) {
         console.log('blanks!')
       } else if (document.querySelector('#name').value && document.querySelector('#photo').value) {
-        let answerArr = []
+        let answers = []
+        let name = document.querySelector('#name').value
+        let photo = document.querySelector('#photo').value
+        let user1 = { name, photo, answers }
         for (let i = 0; i < 10; i++) {
           if (document.getElementById(`q${i + 1}`).value === '') {
-            console.log(`Please answer question ${i + 1}.`)
+            alert(`Please answer question ${i + 1}.`)
             break
           } else {
-            answerArr[i] = parseInt(document.getElementById(`q${i + 1}`).value)
+            answers[i] = document.getElementById(`q${i + 1}`).value
           }
         }
         fetch('/api/friends', {
@@ -31,13 +52,22 @@ module.exports = app => {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            name: document.querySelector('#name').value,
-            photo: document.querySelector('#photo').value,
-            answers: answerArr
+            name: user1.name,
+            photo: user1.photo,
+            answers: user1.answers
           })
         })
-          .then(_ => {
-            compare()
+          .then(user1 => {
+            answers = []
+            document.querySelector('#name').value = ''
+            document.querySelector('#photo').value = ''
+            user1 = {}
+            for (let i = 1; i < 11; i++) {
+              document.getElementById(`q${i}`).value = ''
+            }
+            compare(user1)
+            // put user1 into the friends.js file
+            // modal pop up to display the common friend
           })
           .catch(e => console.log(e))
       }
